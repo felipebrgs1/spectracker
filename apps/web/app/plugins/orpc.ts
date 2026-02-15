c6a41b0071d8b61d24d63593e56bce2990c83d07
felipeb <felipeborgaco@hotmail.com>
2026-02-15 18:39:36 -0300

    init

---
+++ apps/web/app/plugins/orpc.ts
@@ -0,0 +1,24 @@
+import type { AppRouterClient } from "@my-better-t-app/api/routers/index";
+
+import { defineNuxtPlugin } from "#app";
+import { createORPCClient } from "@orpc/client";
+import { RPCLink } from "@orpc/client/fetch";
+import { createTanstackQueryUtils } from "@orpc/tanstack-query";
+
+export default defineNuxtPlugin(() => {
+  const config = useRuntimeConfig();
+  const rpcUrl = `${config.public.serverUrl}/rpc`;
+
+  const rpcLink = new RPCLink({
+    url: rpcUrl,
+  });
+
+  const client: AppRouterClient = createORPCClient(rpcLink);
+  const orpcUtils = createTanstackQueryUtils(client);
+
+  return {
+    provide: {
+      orpc: orpcUtils,
+    },
+  };
+});
